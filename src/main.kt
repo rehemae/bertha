3b38fd21bd8c1401444913d15a630a3d086c634f
rehemae <rehemaephraimnanda@gmail.com>
2022-02-23 13:43:54 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,38 @@
+fun main(){
+  surname("rehema")
+    var reminder= modulus(20,12)
+    println(reminder)
+    var add= sum(4,7,8,9)
+    println(add)
+    println(" I love swimming")
+
+}
+
+fun surname(name: String){
+
+var x = ("Hello" + " " + name)
+    println(x)
+
+    }
+
+
+
+
+
+
+
+fun modulus(num1:Int, num2: Int): Int{
+    var reminder= num1%num2
+    return reminder
+}
+
+
+fun sum(num1: Int, num2: Int, num3: Int, num4: Int):Int{
+    var add= num1+ num2+ num3+ num4
+    return add
+}
+
+
+fun printintrestingfunfactaboutme(fact:String){
+    print("love" + " " +fact)
+}
